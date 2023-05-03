41b452810ad031fd58db6550e398d364eb36e4e5
Frad Rim <fradrim1997@gmail.com>
2023-05-03 15:21:04 +0100

    Automatisation des tests de l'application web Sofia Skills

--- my-app/src/test/java/stepdefinitions/login_stepdefinition.java
+++ my-app/src/test/java/stepdefinitions/login_stepdefinition.java
@@ -8,11 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
 
 
 public class login_stepdefinition {
 public  static WebDriver driver; 
-	
 	
 	@Given("I visit login page")
 	public void i_visit_login_page() {
@@ -42,15 +42,56 @@ public  static WebDriver driver;
 	@Then("I should be in profile page")
 	public void i_should_be_in_profile_page() {
 	   Assert.assertEquals(driver.getTitle(),"Profile" );
-       
+		driver.quit();;
+
 
 	}
 	@Then("Error message is shown {string}")
 	public void error_message_is_shown(String string) {
 		Assert.assertEquals(driver.getTitle(),"Sofia tech" );
 		System.out.println("Erreur");
-		
+		driver.quit();;
 	}
+	
+	
+	@When("I click on the Forgot password button")
+	public void i_click_on_the_Forgot_password_button1() throws InterruptedException {
+		 WebElement getfogotbutton = driver.findElement(By.xpath("//a[@id='forgetpwd']"));
+		    Thread.sleep(1000);
+		    getfogotbutton.click();
+		    Thread.sleep(2000);
+	}
+
+	@Then("I redirected to the password recovery page")
+	public void i_redirected_to_the_password_recovery_page() {
+		   Assert.assertEquals(driver.getTitle(),"Sofia tech" );
+
+	}
+
+	@And("I enter email address {string}")
+	public void i_enter_email_address(String email) throws InterruptedException {
+		 WebElement getinputemail =  driver.findElement(By.id("email"));
+		  getinputemail.sendKeys(email);
+		  Thread.sleep(2000);
+		 
+	}
+
+	@And("I click on the submit button")
+	public void i_click_on_the_submit_button() throws InterruptedException {
+		WebElement getsubmitbutton = driver.findElement(By.xpath("//button[@type='submit']"));
+	    Thread.sleep(1000);
+	    getsubmitbutton.click();
+	    Thread.sleep(2000);
+	}
+
+	@Then("An email is sent with an {int} character password")
+	public void an_email_is_sent_with_an_character_password(Integer int1) {
+		Assert.assertEquals(driver.getTitle(),"Sofia tech" );
+		System.out.println("OK");
+		driver.quit();;
+
+	}
+
 	
 	}
 	
